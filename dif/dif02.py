f5eb042730b35e477593dd61818696f51f891cfb
Nattawat66 <nattawat.ma.66@ubu.ac.th>
2024-02-20 15:52:20 +0700

    newagain

--- dif/dif02.py
+++ dif/dif02.py
@@ -1,13 +1,8 @@
 import torch
 from diffusers import DiffusionPipeline as DP
-
 from PIL import Image
 
 def text_to_image(text, diffuser_model):
-    #diffuser = diffusers.load_diffuser(diffuser_model)
-    # image_data = diffuser.generate(text)
-    # image = Image.fromarray(image_data)
-    # image.show()
     dp = DP.from_pretrained("runwayml/stable-diffusion-v1-5")
     image_data = dp(text).images[0]
     image = Image.fromarray(image_data)
--- streamlit/index3.py
+++ streamlit/index3.py
@@ -8,9 +8,10 @@ st.button('หน้าแรก')
 
 st.title('OOP in ML')
 mainpic = st.image('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExemV2ZG94dG40M2ZsMGkwOWlieXJzZmp5Nno1OWN1ZHhyN2txc3hmeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/F6ZC06fX688qk/giphy.gif')
-text = st.text_input("Your Text: ")
+text = st.text_input("Your Text Gen Image: ")
 
 if text:
+    print("Work")
     dp = DP.from_pretrained("runwayml/stable-diffusion-v1-5")
     image_data = dp(text).images[0]
     image = Image.fromarray(image_data)
